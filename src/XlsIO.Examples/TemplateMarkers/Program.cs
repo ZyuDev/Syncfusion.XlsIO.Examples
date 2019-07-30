daf533103f31e58dce54b7be2b89372158d5b8ad
MikhaZyu <MikhaZyu@gmail.com>
2019-07-30 18:56:57 +0300

    Template markers
    Example of using template markers in Excel file

--- src/XlsIO.Examples/TemplateMarkers/Program.cs
+++ src/XlsIO.Examples/TemplateMarkers/Program.cs
@@ -1,8 +1,5 @@
-using System;
+using Syncfusion.XlsIO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TemplateMarkers
 {
@@ -10,12 +7,37 @@ namespace TemplateMarkers
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            // Init rows collection.
+            var records = new List<ReportRow>()
+            {
+                new ReportRow() {Name = "Product 1", Quantity = 2, Price = 100},
+                new ReportRow() {Name = "Product 2", Quantity = 1, Price = 200},
+                new ReportRow() {Name = "Product 3", Quantity = 5, Price = 300},
+                new ReportRow() {Name = "Product 4", Quantity = 10, Price = 150},
+                new ReportRow() {Name = "Product 5", Quantity = 7, Price = 100}
+            };
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            //Creates a new instance for ExcelEngine
+            ExcelEngine excelEngine = new ExcelEngine();
+
+            //Loads or open an existing workbook through Open method of IWorkbooks
+            var inputFileName = @"Template.xlsx";
+            var resultFileName = @"Report.xlsx";
+
+            IWorkbook workbook = excelEngine.Excel.Workbooks.Open(inputFileName);
+
+            //Create Template Marker Processor
+            ITemplateMarkersProcessor marker = workbook.CreateTemplateMarkersProcessor();
+
+            //Add collections to the marker variables where the name should match with input template
+            marker.AddVariable("records", records);
+
+            //Process the markers in the template
+            marker.ApplyMarkers();
+
+            workbook.SaveAs(resultFileName);
+
+
         }
     }
 }
---
+++ src/XlsIO.Examples/TemplateMarkers/ReportRow.cs
@@ -0,0 +1,9 @@
+﻿namespace TemplateMarkers
+{
+    public class ReportRow
+    {
+        public string Name { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
